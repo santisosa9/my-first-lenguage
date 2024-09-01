8144d0d4e55c1f542012f15766a81e394aa22ad8
Nicolás Mercado <nicolasmercado452@gmail.com>
2024-09-01 00:30:07 -0300

    Modify main.c and add ii.txt to test the AST

--- src/main.c
+++ src/main.c
@@ -3,14 +3,29 @@
 #include <string.h>
 #include "../headers/ast.h"
 #include "../headers/utils.h"
+#include <stdbool.h>
 
 
 int main(int argc, char const *argv[])
 {
-    Info* info = new_info(10, INT, INT);
-    AST* left = new_tree(new_info(20, INT, INT), new_node(new_info(40, INT, INT)), new_node(new_info(50, INT, INT)));
-    AST* right = new_node(new_info(30, INT, INT));
-    AST* tree = new_tree(info, left, right);
+    Info* a_info = new_info(MAIN, INT, 20, "a", 1, 0);
+    Info* b_info = new_info(MAIN, INT, 20, "b", 1, 0);
+    Info* c_info = new_info(MAIN, INT, 20, "c", 1, 0);
+    Info* d_info = new_info(MAIN, INT, 20, "d", 1, 0);
+    Info* e_info = new_info(MAIN, INT, 20, "e", 1, 0);
+    Info* f_info = new_info(MAIN, INT, 20, "f", 1, 0);
+    Info* g_info = new_info(MAIN, INT, 20, "g", 1, 0);
+
+    AST* g = new_node(g_info);
+    AST* f = new_node(f_info);
+    AST* e = build_root(f, e_info, g);
+    AST* d = new_node(d_info);
+    AST* c = new_node(c_info);
+    AST* b = build_root(d, b_info, e);
+    AST* a = build_root(b, a_info, c);
+
+    AST* tree = a;
+
     print_tree(tree);
     free_tree(tree);
     return 0;
